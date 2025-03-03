ea317159698fe96e4dabbb61d1ab13810f56a640
juandejunin <juandejunin75@gmail.com>
2025-03-03 11:37:09 +0100

    Add hamburger menu transitions and fix icon rotation for symmetric x efect

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,52 @@
+// /** @type {import('tailwindcss').Config} */
+// export default {
+//   content: ['./src/**/*.{astro,html,js,jsx,ts,tsx}'],
+//   theme: {
+//     extend: {
+//       colors: {
+//         jaPrimary: '#1E90FF',
+//         jaSecondary: '#333'
+//       }
+//     }
+//   },
+//   plugins: []
+// };
+
+// /** @type {import('tailwindcss').Config} */
+// module.exports = {
+//   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+//   theme: {
+//     extend: {
+//       fontFamily: {
+//         inconsolata: ['Inconsolata', 'sans-serif'],
+//       },
+//     },
+//   },
+//   plugins: [
+//     require('@iconify/tailwind').addDynamicIconSelectors(),
+//   ],
+// };
+
 /** @type {import('tailwindcss').Config} */
-export default {
-  content: ['./src/**/*.{astro,html,js,jsx,ts,tsx}'],
+module.exports = {
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
     extend: {
-      colors: {
-        jaPrimary: '#1E90FF',
-        jaSecondary: '#333'
-      }
-    }
+      fontFamily: {
+        inconsolata: ['Inconsolata', 'sans-serif'],
+      },
+      animation: {
+        'fade-in': 'fadeIn 0.5s ease-in forwards',
+      },
+      keyframes: {
+        fadeIn: {
+          '0%': { opacity: '0' },
+          '100%': { opacity: '1' },
+        },
+      },
+    },
   },
-  plugins: []
+  plugins: [
+    require('@iconify/tailwind').addDynamicIconSelectors(),
+  ],
 };
